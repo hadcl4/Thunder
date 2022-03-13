baadb5e399fa645aa8bc6118f7bc78efd566bc23
hadcl4 <hadcl4.studios@gmail.com>
2022-03-13 12:43:43 -0500

    Trying to get the updater for #2 to work still...

--- main.py
+++ main.py
@@ -303,7 +303,6 @@ def setup_game():
 def thunder_update():
     command = f'gnome-terminal --window --command="/bin/bash /home/$USER/Thunder/update"'
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    app.destroy()
 def thunder_docs():
     command = f'gnome-terminal --window --command="/home/$USER/Thunder/thunder-cli --docs"'
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
