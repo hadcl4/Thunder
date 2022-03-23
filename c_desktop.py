6786a1e8fdecf58a4e8922901883c18fc6ae3c34
hadcl4 <hadcl4.studios@gmail.com>
2022-03-23 18:57:35 -0500

    Fix a bug with the desktop shortcut

--- c_desktop.py
+++ c_desktop.py
@@ -6,7 +6,7 @@ from os.path import expanduser
 user = sys.argv[1]
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-f_content = "[Desktop Entry]\nName=Thunder\nComment=Thunder makes using your ARM SBC as a Gaming PC easy and simple. With all your PC games in one place, easily run any game for almost any platform.\nExec="+dir_path+"/start\nIcon="+dir_path+"/logo.png\nTerminal=false\nType=Application\nCategories=Game;\nStartupNotify=true"
+f_content = "[Desktop Entry]\nName=Thunder\nComment=Thunder makes using your ARM SBC as a Gaming PC easy and simple. With all your PC games in one place, easily run any game for almost any platform.\nExec="+dir_path+"/start\nIcon="+dir_path+"/logo.png\nTerminal=false\nType=Application\nCategories=Game;\nStartupNotify=false"
 
 x_dir = "/usr/share/applications/thunder.desktop"
 
