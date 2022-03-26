53e2e8ab9f0ac67d0ced7cab9558c69c1576bd6a
hadcl4 <hadcl4.studios@gmail.com>
2022-03-26 13:36:39 -0500

    Fix a dependency issue with Steam on RK3399, possibly other devices

--- database/steam.sh
+++ database/steam.sh
@@ -7,6 +7,6 @@ sudo sed -i 's/set -e/set -e\nexport STEAMOS=1\nexport STEAM_RUNTIME=1/' /usr/li
 mkdir -p ~/.local/share/Steam/config # Prepare for the next hack.
 wget https://raw.githubusercontent.com/Botspot/Steam-RPi/main/DialogConfig.vdf -O ~/.local/share/Steam/config/DialogConfig.vdf # Now let's hack Steam some more!
 echo Now for the dependencies... # Without them it might not work.
-sudo apt install libnss3:armhf libnm0:armhf libdbus-glib-1-2:armhf libudev1:armhf libnspr4:armhf libgudev-1.0-0:armhf libusb-1.0-0:armhf libappindicator1:armhf # These are for Hirsute, it might not work on other distros.
+sudo apt install libnss3:armhf libnm0:armhf libdbus-glib-1-2:armhf libudev1:armhf libnspr4:armhf libgudev-1.0-0:armhf libusb-1.0-0:armhf libappindicator1:armhf libsm6:armhf libxtst6:armhf libice6:armhf # This command has only been tested on Focal and Hisute, and might not work on other distros!
 echo If you need help with Steam, view steampowered.com or box86.org
 echo Steam installed!
