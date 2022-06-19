51817ab5206ed81db52d6450fb3fa237ab3522e7
hadcl4 <hadcl4.studios@gmail.com>
2022-06-19 16:49:12 -0500

    Custom Mesa and GALLIUM_HUD now work with Virtual Cartridges

--- read.py
+++ read.py
@@ -3,11 +3,27 @@ import configparser
 from guizero import *
 import subprocess
 from PIL import Image
+import os
 
 argv = sys.argv
 argc = len(sys.argv)
+home_path = os.getenv("HOME")
+mesaconfigur = configparser.ConfigParser()
+mesaread = mesaconfigur.read(home_path+"/.thunder/mesa.cfg")
+mesa32 = mesaconfigur.get("mesa","ARMHF")
+mesa64 = mesaconfigur.get("mesa","ARM64")
+mesaon = mesaconfigur.get("mesa","ON")
+galliumon = mesaconfigur.get("mesa","GALLIUM_HUD")
+gallium = open(home_path+"/.thunder/gallium.cfg", "r")
+gallium_args = gallium.read()
+gallium.close()
 
 if argc == 2:
+    args = ""
+    if galliumon == "ON":
+        args += " GALLIUM_HUD="+gallium_args+" "
+    if mesaon == "1":
+        args += " LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"+mesa64+":"+mesa32+" "
     configur = configparser.ConfigParser()
     configur.read(argv[1])
     name = configur.get("Game","NM")
@@ -16,17 +32,17 @@ if argc == 2:
     runner = configur.get("Game","RN")
     def gamerun():
         if runner == "linux":
-            command = f''+exec
+            command = f''+args+' '+exec
         if runner == "wine":
-            command = f'wine '+exec
+            command = f''+args+' wine '+exec
         if runner == "steam":
-            command = f'steam steam://rungameid/'+exec
+            command = f''+args+' steam steam://rungameid/'+exec
         if runner == "browser":
-            command = f'x-www-browser '+exec
+            command = f''+args+' x-www-browser '+exec
         if runner == "flatpak":
-            command = f'flatpak run '+exec
+            command = f''+args+' flatpak run '+exec
         if runner == "mednafen":
-            command = f'mednafen '+exec
+            command = f''+args+' mednafen '+exec
         process = subprocess.Popen(command, stderr=True, stdout=True, shell=True)
     app = App(title="Thunder - "+name, width=600,bg="white")
     app.icon = icon
