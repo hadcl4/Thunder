995cdf284bfa8d45b3b8d0231b05febcfd2ac633
hadcl4 <hadcl4.studios@gmail.com>
2022-10-22 16:56:46 -0500

    Simplify Steam installation, now using Box86's install script

--- database/steam.sh
+++ database/steam.sh
@@ -2,17 +2,9 @@
 read -p "You don't need to run this script if you installed Steam during setup. Want to continue? Press enter. Otherwise press Ctrl+C."
 boxcheck=$(command -v box86)
 if [[ $boxcheck == "/usr/local/bin/box86" ]]; then
-cd ~
-wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb
-sudo dpkg -i steam.deb # This will work because Steam is a hybrid amd64/i386 app, so it's marked as "all" architectures supported, you can't have 2!
-rm ~/steam.deb # Clean it up afterwards
-sudo sed -i 's/set -e/set -e\nexport STEAMOS=1\nexport STEAM_RUNTIME=1/' /usr/lib/steam/bin_steam.sh # Let's sed hack some stuff!
-mkdir -p ~/.local/share/Steam/config # Prepare for the next hack.
-wget https://raw.githubusercontent.com/Botspot/Steam-RPi/main/DialogConfig.vdf -O ~/.local/share/Steam/config/DialogConfig.vdf # Now let's hack Steam some more!
-echo Now for the dependencies... # Without them it might not work.
-sudo apt install libnss3:armhf libnm0:armhf libdbus-glib-1-2:armhf libudev1:armhf libnspr4:armhf libgudev-1.0-0:armhf libusb-1.0-0:armhf libappindicator1:armhf libsm6:armhf libxtst6:armhf libice6:armhf # This command has only been tested on Focal and Hisute, and might not work on other distros!
-echo If you need help with Steam, view steampowered.com or box86.org
-echo Steam installed!
+cd ~/box86
+echo "Running Box86's Steam install script..."
+bash install_steam.sh
 else
 echo "Please install Box86 before installing Steam."
 echo "Don't want to have to compile Box86 by yourself? Check out Box86-Manager:"
